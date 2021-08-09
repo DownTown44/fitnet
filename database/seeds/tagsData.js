597ef8d318aae93433e5f1ae83317fc76d587193
apor4444 <apor4444@gmail.com>
2021-08-09 09:34:00 +0300

    V1_6 Tags seed created

---
+++ database/seeds/tagsData.js
@@ -0,0 +1,33 @@
+const data = [
+  {
+    name: 'Football',
+    icon: 'none'
+  },
+  {
+    name: 'Basketball',
+    icon: 'none'
+  },
+  {
+    name: 'Handball',
+    icon: 'none'
+  },
+  {
+    name: 'Table tennis',
+    icon: 'none'
+  },
+  {
+    name: 'Tennis',
+    icon: 'none'
+  },
+  {
+    name: 'Squash',
+    icon: 'none'
+  },
+  {
+    name: 'Volleyball',
+    icon: 'none'
+  },
+];
+  
+export default data;
+  

bdfdd0af17ba5fb0ab261ac9494bcc8b5a3cb97c
apor4444 <apor4444@gmail.com>
2021-08-09 11:18:09 +0300

    V1_6 Created multiple seeds bookings, facilities, facilityTags etc.

---
+++ database/seeds/fieldsData.js
@@ -0,0 +1,18 @@
+const fieldsData = [
+  {
+    facility_id: 1,
+    picture: 'none',
+    name: 'Footbal field',
+    price: 80,
+    description: 'Cool open football field, with lights.'
+  },
+  {
+    facility_id: 2,
+    picture: 'none',
+    name: 'Footbal field',
+    price: 120,
+    description: 'Cool closed football field, with air conditioner.'
+  },
+];
+
+export { fieldsData };

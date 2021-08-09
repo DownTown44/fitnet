29bf7d1cde1d9c0e922c189efe25720400db680f
Kis Csabika <kcsabika1013@gmail.com>
2021-08-09 09:25:05 +0300

    V1_6 User, Groups, Group members seeds done

--- database/seeds/groupsData.js
+++ database/seeds/groupsData.js
@@ -5,8 +5,22 @@ const data = [
     name: 'SoccerGroup',
     description: 'A group for soccer players',
     picture: 'none'
-  }
-]
+  },
+  {
+    user_id: 2,
+    accessibility_id: '2',
+    name: 'Basket Case',
+    description: 'Basket ball goup',
+    picture: 'none'
+  },
+  {
+    user_id: 3,
+    accessibility_id: '3',
+    name: 'Mini golfers',
+    description: 'We are invisible golfers',
+    picture: 'none'
+  },
+];
   
 export default data;
   

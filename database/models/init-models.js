cf41659071009f85f433dfced331c481a5c07652
apor4444 <apor4444@gmail.com>
2021-08-09 12:07:18 +0300

    V1_6 execsql removed, formatting

--- database/models/init-models.js
+++ database/models/init-models.js
@@ -18,65 +18,86 @@ import _tags from  "./tags.js";
 import _users from  "./users.js";
 
 export default function initModels(sequelize) {
-  var accessibilities = _accessibilities.init(sequelize, DataTypes);
-  var bookings = _bookings.init(sequelize, DataTypes);
-  var event_types = _event_types.init(sequelize, DataTypes);
-  var events = _events.init(sequelize, DataTypes);
-  var facilities = _facilities.init(sequelize, DataTypes);
-  var facility_pictures = _facility_pictures.init(sequelize, DataTypes);
-  var facility_tags = _facility_tags.init(sequelize, DataTypes);
-  var fields = _fields.init(sequelize, DataTypes);
-  var group_members = _group_members.init(sequelize, DataTypes);
-  var group_tags = _group_tags.init(sequelize, DataTypes);
-  var groups = _groups.init(sequelize, DataTypes);
-  var opening_hours = _opening_hours.init(sequelize, DataTypes);
-  var reviews = _reviews.init(sequelize, DataTypes);
-  var roles = _roles.init(sequelize, DataTypes);
-  var tags = _tags.init(sequelize, DataTypes);
-  var users = _users.init(sequelize, DataTypes);
+  const accessibilities = _accessibilities.init(sequelize, DataTypes);
+  const bookings = _bookings.init(sequelize, DataTypes);
+  const event_types = _event_types.init(sequelize, DataTypes);
+  const events = _events.init(sequelize, DataTypes);
+  const facilities = _facilities.init(sequelize, DataTypes);
+  const facility_pictures = _facility_pictures.init(sequelize, DataTypes);
+  const facility_tags = _facility_tags.init(sequelize, DataTypes);
+  const fields = _fields.init(sequelize, DataTypes);
+  const group_members = _group_members.init(sequelize, DataTypes);
+  const group_tags = _group_tags.init(sequelize, DataTypes);
+  const groups = _groups.init(sequelize, DataTypes);
+  const opening_hours = _opening_hours.init(sequelize, DataTypes);
+  const reviews = _reviews.init(sequelize, DataTypes);
+  const roles = _roles.init(sequelize, DataTypes);
+  const tags = _tags.init(sequelize, DataTypes);
+  const users = _users.init(sequelize, DataTypes);
 
   events.belongsTo(accessibilities, { as: "accessibility", foreignKey: "accessibility_id"});
   accessibilities.hasMany(events, { as: "events", foreignKey: "accessibility_id"});
+
   groups.belongsTo(accessibilities, { as: "accessibility", foreignKey: "accessibility_id"});
   accessibilities.hasMany(groups, { as: "groups", foreignKey: "accessibility_id"});
+
   events.belongsTo(event_types, { as: "type", foreignKey: "type_id"});
   event_types.hasMany(events, { as: "events", foreignKey: "type_id"});
+
   bookings.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(bookings, { as: "bookings", foreignKey: "facility_id"});
+
   facility_pictures.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(facility_pictures, { as: "facility_pictures", foreignKey: "facility_id"});
+
   facility_tags.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(facility_tags, { as: "facility_tags", foreignKey: "facility_id"});
+
   fields.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(fields, { as: "fields", foreignKey: "facility_id"});
+
   opening_hours.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(opening_hours, { as: "opening_hours", foreignKey: "facility_id"});
+
   reviews.belongsTo(facilities, { as: "facility", foreignKey: "facility_id"});
   facilities.hasMany(reviews, { as: "reviews", foreignKey: "facility_id"});
+
   bookings.belongsTo(fields, { as: "field", foreignKey: "field_id"});
   fields.hasMany(bookings, { as: "bookings", foreignKey: "field_id"});
+
   bookings.belongsTo(groups, { as: "group", foreignKey: "group_id"});
   groups.hasMany(bookings, { as: "bookings", foreignKey: "group_id"});
+
   group_members.belongsTo(groups, { as: "group", foreignKey: "group_id"});
   groups.hasMany(group_members, { as: "group_members", foreignKey: "group_id"});
+  
   group_tags.belongsTo(groups, { as: "group", foreignKey: "group_id"});
   groups.hasMany(group_tags, { as: "group_tags", foreignKey: "group_id"});
+
   users.belongsTo(roles, { as: "role", foreignKey: "role_id"});
   roles.hasMany(users, { as: "users", foreignKey: "role_id"});
+
   facility_tags.belongsTo(tags, { as: "tag", foreignKey: "tag_id"});
   tags.hasMany(facility_tags, { as: "facility_tags", foreignKey: "tag_id"});
+
   group_tags.belongsTo(tags, { as: "tag", foreignKey: "tag_id"});
   tags.hasMany(group_tags, { as: "group_tags", foreignKey: "tag_id"});
+
   bookings.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasMany(bookings, { as: "bookings", foreignKey: "user_id"});
+
   events.belongsTo(users, { as: "owner", foreignKey: "owner_id"});
   users.hasMany(events, { as: "events", foreignKey: "owner_id"});
+
   facilities.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasMany(facilities, { as: "facilities", foreignKey: "user_id"});
+
   group_members.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasMany(group_members, { as: "group_members", foreignKey: "user_id"});
+
   groups.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasOne(groups, { as: "group", foreignKey: "user_id"});
+  
   reviews.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasMany(reviews, { as: "reviews", foreignKey: "user_id"});
 

64a66ebbc1fe00edc21b725881775354ef255c84
Kis Csabika <kcsabika1013@gmail.com>
2021-08-09 11:17:23 +0300

    V1_6 Reviews, Opening hours seeds done

---
+++ database/seeds/openingHoursData.js
@@ -0,0 +1,44 @@
+const openingHoursData = [
+  {
+    facility_id: 1,
+    day: 1,
+    opening: '08:00:00',
+    closing: '02:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 2,
+    opening: '08:00:00',
+    closing: '02:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 3,
+    opening: '08:00:00',
+    closing: '02:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 4,
+    opening: '08:00:00',
+    closing: '02:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 5,
+    opening: '08:00:00',
+    closing: '02:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 6,
+    opening: '10:00:00',
+    closing: '00:00:00'
+  },
+  {
+    facility_id: 1,
+    day: 7,
+    opening: '12:00:00',
+    closing: '10:00:00'
+  }
+];

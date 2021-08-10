0b0cb83ade29944ef3169560088acd0b57bdfd77
apor4444 <apor4444@gmail.com>
2021-08-10 11:06:40 +0300

    V1_12 Calendar component added

---
+++ src/components/CustomCalendar/CustomCalendar.jsx
@@ -0,0 +1,15 @@
+import Calendar from 'react-calendar';
+
+const CustomCalendar = () => {
+  // TODO: when server is ready, we need to render notifications on the days and 
+  // add onclick to them
+  // TODO: Calendar needs styling
+  // https://github.com/wojtekmaj/react-calendar
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+}
+
+export default CustomCalendar;

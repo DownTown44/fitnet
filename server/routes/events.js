1433f54d7dfadd939d02bb23e6e0913084b7a27d
Kis Csabika <kcsabika1013@gmail.com>
2021-08-19 09:46:39 +0300

    V1_26 Event DTO done

--- server/routes/events.js
+++ server/routes/events.js
@@ -4,6 +4,31 @@ import { createEvent, getEverythingOf } from '../../database/dbHandler.js';
 
 const router = express.Router();
 
+const eventDTO = (data) => {
+  const dto = {};
+  const {
+    event_id,
+    accessibility_id,
+    type_id,
+    owner_id,
+    name,
+    description,
+    address,
+    start_date
+  } = data;
+  
+  dto.eventId = event_id;
+  dto.accessibilityId = accessibility_id;
+  dto.typeId = type_id;
+  dto.ownerId = owner_id;
+  dto.name = name;
+  dto.description = description;
+  dto.address = address;
+  dto.startDate = start_date;
+
+  return dto;
+}
+
 router.post('/', async (req, res) => {
   const data = req.body;
   try {
@@ -21,6 +46,9 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
   try {
     const result = await getEverythingOf('events');
+    result.forEach((element, index, array) => {
+      array[index] = eventDTO(element);
+    });
     res.json(result);
   } catch (error) {
     console.log(error);

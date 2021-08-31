389118e4ad0047fe84d59fe51c71134720dc710a
Kis Csabika <kcsabika1013@gmail.com>
2021-08-31 13:08:21 +0300

    V1_44 If statement moved to separate function

--- server/middleware/signupValidator.js
+++ server/middleware/signupValidator.js
@@ -1,6 +1,6 @@
 import validator from 'validator';
 
-const signupValidator = (req, res, next) => {
+const validateInput = (data) => {
   const {
     email,
     first_name,
@@ -8,7 +8,7 @@ const signupValidator = (req, res, next) => {
     phone_number,
     password,
     repeated_password,
-  } = req.body;
+  } = data;
 
   if (!validator.isEmail(email) ||
   !validator.isAlpha(first_name, 'hu-HU') ||
@@ -16,9 +16,18 @@ const signupValidator = (req, res, next) => {
   !validator.isMobilePhone(phone_number, 'ro-RO') ||
   !validator.isStrongPassword(password, {minSymbols: 0}) ||
   !validator.equals(repeated_password, password)) {
+    return false;
+  }
+
+  return true;
+}
+
+const signupValidator = (req, res, next) => {
+
+  if (!validateInput(req.body)) {
     res.status(400);
     res.json({message: 'One of the following datas is not valid'});
-
+    
     return;
   }
 

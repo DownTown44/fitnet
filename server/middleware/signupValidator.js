de1354b1b741c0b56fef50f9b0179bc83f805142
Kis Csabika <kcsabika1013@gmail.com>
2021-08-31 13:48:18 +0300

    V1_44 Signup validation simplified

--- server/middleware/signupValidator.js
+++ server/middleware/signupValidator.js
@@ -1,6 +1,46 @@
 import validator from 'validator';
 
-const validateInput = (data) => {
+const checkName = (name) => {
+  if (validator.isAlpha(name, 'hu-HU')) {
+    return true;
+  }
+
+  return false;
+}
+
+const checkEmail = (email) => {
+  if (validator.isEmail(email)) {
+    return true;
+  }
+
+  return false;
+}
+
+const checkPhoneNumber = (phoneNumber) => {
+  if (validator.isMobilePhone(phoneNumber, 'ro-RO')) {
+    return true;
+  }
+
+  return false;
+}
+
+const checkPwd = (pwd) => {
+  if (validator.isStrongPassword(pwd, {minSymbols: 0})) {
+    return true;
+  }
+
+  return false;
+}
+
+const checkEquality = (pwd, repeatedPwd) => {
+  if (validator.equals(repeatedPwd, pwd)) {
+    return true;
+  }
+
+  return false;
+}
+
+const validateAll = (data) => {
   const {
     email,
     first_name,
@@ -10,21 +50,21 @@ const validateInput = (data) => {
     repeated_password,
   } = data;
 
-  if (!validator.isEmail(email) ||
-  !validator.isAlpha(first_name, 'hu-HU') ||
-  !validator.isAlpha(last_name, 'hu-HU') ||
-  !validator.isMobilePhone(phone_number, 'ro-RO') ||
-  !validator.isStrongPassword(password, {minSymbols: 0}) ||
-  !validator.equals(repeated_password, password)) {
-    return false;
+  if (checkEmail(email) &&
+      checkName(first_name) &&
+      checkName(last_name) &&
+      checkPhoneNumber(phone_number) &&
+      checkPwd(password) &&
+      checkEquality(password, repeated_password)) {
+    return true;
   }
 
-  return true;
+  return false;
 }
 
 const signupValidator = (req, res, next) => {
 
-  if (!validateInput(req.body)) {
+  if (!validateAll(req.body)) {
     res.status(400);
     res.json({message: 'One of the following datas is not valid'});
     

c4d8dd7b6ea1861f1117735a55d3451525dec37b
Kis Csabika <kcsabika1013@gmail.com>
2021-09-02 15:29:21 +0300

    V1_60 Icon and button design system done

--- src/designSystem/DesignSystem.jsx
+++ src/designSystem/DesignSystem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ColorBox from './ColorBox/ColorBox';
+import Icon from '@material-ui/core/Icon';
+import Button from '../components/UI/Button';
 
 const DesignSystem = () => {
   return (
     <div className="design-system">
-
       <div className="design-system__colors">
         <h2>Color system</h2>
         <div className="design-system__color-container">
@@ -48,6 +49,129 @@ const DesignSystem = () => {
             <ColorBox className="gradient-blue-green" colorName="Gradient blue to green" />
           </div>
         </div>
+      </div>
+
+      <div className="design-system__icons">
+
+        <h2>Iconography</h2>
+        <div className="design-system__icon-container">
+          <h3 className="design-system__icon-container-title">Icons</h3>
+          <div className="design-system__icon-container-icons">
+            <Icon className="md30">favorite_border</Icon>
+            <Icon className="md30">favorite</Icon>
+            <Icon className="md30">room</Icon>
+            <Icon className="md30">search</Icon>
+            <Icon className="md30">settings</Icon>
+            <Icon className="md30">sports_basketball</Icon>
+            <Icon className="md30">sports_golfball</Icon>
+            <Icon className="md30">sports_soccer</Icon>
+            <Icon className="md30">sports_tennis</Icon>
+            <Icon className="md30">sports_volleyball</Icon>
+            <Icon className="md30">view_headline</Icon>
+            <Icon className="md30">watch_later</Icon>
+            <Icon className="md30">add</Icon>
+            <Icon className="md30">add_circle_outline</Icon>
+            <Icon className="md30">arrow_back</Icon>
+            <Icon className="md30">arrow_forward</Icon>
+            <Icon className="md30">bookmark_border</Icon>
+            <Icon className="md30">chevron_left</Icon>
+            <Icon className="md30">dashboard</Icon>
+            <Icon className="md30">delete_outline</Icon>
+            <Icon className="md30">done</Icon>
+            <Icon className="md30">event_note</Icon>
+            <Icon className="md30">expand_less</Icon>
+            <Icon className="md30">expand_more</Icon>
+            <Icon className="md30">filter_list</Icon>
+            <Icon className="md30">help_outline</Icon>
+            <Icon className="md30">info</Icon>
+            <Icon className="md30">login</Icon>
+            <Icon className="md30">logout</Icon>
+            <Icon className="md30">mail_outline</Icon>
+            <Icon className="md30">manage_accounts</Icon>
+            <Icon className="md30">menu</Icon>
+            <Icon className="md30">navigate_next</Icon>
+            <Icon className="md30">notifications_none</Icon>
+            <Icon className="md30">question_answer</Icon>
+            <Icon className="md30">report_problem</Icon>
+            <Icon className="md30">person</Icon>
+            <Icon className="md30">visibility</Icon>
+            <Icon className="md30">face</Icon>
+            <Icon className="md30">bookmark</Icon>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="design-system__buttons">
+
+        <h2>Buttons</h2>
+        <div className="design-system__button-container">
+
+          <div className="design-system__button-container-buttons">
+            <h3 className="design-system__button-container-title">Active</h3>
+            <Button additionalClass="normal">
+              BUTTON
+            </Button>
+            <Button additionalClass="outlined">
+              BUTTON
+            </Button>
+            <Button additionalClass="normal-iconed">
+              BUTTON
+              <Icon>done</Icon>
+            </Button>
+            <Button additionalClass="outlined-iconed">
+              <Icon>done</Icon>
+              BUTTON
+            </Button>
+            <Button additionalClass="icon-button">
+              <Icon>menu</Icon>
+            </Button>
+          </div>
+
+          <div className="design-system__button-container-buttons">
+            <h3 className="design-system__button-container-title">Pressed</h3>
+            <Button additionalClass="normal-pressed">
+              BUTTON
+            </Button>
+            <Button additionalClass="outlined-pressed">
+              BUTTON
+            </Button>
+            <Button additionalClass="normal-iconed-pressed">
+              BUTTON
+              <Icon>done</Icon>
+            </Button>
+            <Button additionalClass="outlined-iconed-pressed">
+              <Icon>done</Icon>
+              BUTTON
+            </Button>
+            <Button additionalClass="icon-button-pressed">
+              <Icon>menu</Icon>
+            </Button>
+          </div>
+
+          <div className="design-system__button-container-buttons">
+            <h3 className="design-system__button-container-title">Disabled</h3>
+            <Button additionalClass="normal" isDisabled={true}>
+              BUTTON
+            </Button>
+            <Button additionalClass="outlined" isDisabled={true}>
+              BUTTON
+            </Button>
+            <Button additionalClass="normal-iconed" isDisabled={true} onClick={() => {console.log('alma');}}>
+              BUTTON
+              <Icon>done</Icon>
+            </Button>
+            <Button additionalClass="outlined-iconed" isDisabled={true}>
+              <Icon>done</Icon>
+              BUTTON
+            </Button>
+            <Button additionalClass="icon-button" isDisabled={true}>
+              <Icon>menu</Icon>
+            </Button>
+          </div>
+
+        </div>
+
       </div>
 
     </div>

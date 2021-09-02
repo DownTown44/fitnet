64c9a24a7971ba42260e6b52bf798350e1a105f8
apor4444 <apor4444@gmail.com>
2021-09-02 12:45:54 +0300

    V1_46 Facility view done

--- src/components/Carousel/ImageSlider.jsx
+++ src/components/Carousel/ImageSlider.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {MdKeyboardArrowRight, MdKeyboardArrowLeft} from 'react-icons/md';
 
 // Slides are the SliderData array, which stores the image objects
-const ImageSlider = ({ slides }) => {
+const ImageSlider = (props) => {
   const [current, setCurrent] = useState(0);
-  const length = slides.length;
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    setSlides(props.slides);
+  }, [props.slides]);
 
   // Sets the current slide to the next
   // If the current is the last one, than sets it to the first
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent(current === slides.length - 1 ? 0 : current + 1);
   }
 
   // Sets the current slide to the previous
   // If the current slide is the first one, than sets it to the last
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent(current === 0 ? slides.length - 1 : current - 1);
   }
 
   if (!Array.isArray(slides) || slides.length <= 0) {
@@ -31,7 +35,7 @@ const ImageSlider = ({ slides }) => {
         return (
           <div className={index === current ? "active slide" : "slide"} key={index}>
             {/* Render the image, if that is the current */}
-            {index === current && (<img src={slide.image} alt={index} className="image" />)}
+            {index === current && (<img src={slide.picture} alt={index} className="image" />)}
           </div>
         );
       })}
@@ -40,7 +44,7 @@ const ImageSlider = ({ slides }) => {
 }
 
 ImageSlider.propTypes = {
-  slides: PropTypes.array.isRequired,
+  slides: PropTypes.object.isRequired,
 }
 
 export default ImageSlider;
--- src/components/Carousel/SliderData.jsx
+++
@@ -1,5 +0,0 @@
-/* SliderData is an dedicated list to store image objets, received from the server
- * Object formats: 
- *  { image: "source" }
-*/
-export const SliderData = [];
